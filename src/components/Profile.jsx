9c96d9c33efedb2f03e589c6b5a7a72213c7d9c4
colt759 <colt8800@outlook.com>
2020-10-12 13:10:09 -0500

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img src="https://bipbap.ru/wp-content/uploads/2017/10/0_8eb56_842bba74_XL-640x400.jpg" alt=""/>
+        </div>
+        <div>
+            <img src="https://pbs.twimg.com/profile_images/808683397702881280/D7SkGM2B.jpg" alt=""/>
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
